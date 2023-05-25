c44e19606094201a10ac8b135afaefcc311759f9
owls250 <seungar108@gmail.com>
2023-05-25 10:38:46 -0400

    export

--- scheduleCollage.js
+++ scheduleCollage.js
@@ -24,6 +24,9 @@ async function handleFiles() {
   // display the array in a table
   displayarray( sorted_assignments);
 
+  // export
+  array_to_csv( allAssignments);
+
 }
 
 
@@ -39,7 +42,7 @@ async function csvtoarray( csvfile) {
       const reader = new FileReader(); // instantiate new Filereader
 
       reader.onload = e => {
-        resolve(process(reader.result));
+        resolve(process(reader.result, csvfile.name.replace(".csv", "")));
         // https://stackoverflow.com/questions/13729301/html5-file-api-how-to-see-the-result-of-readastext
       };
 
@@ -51,23 +54,29 @@ async function csvtoarray( csvfile) {
     });  
     // https://developer.mozilla.org/en-US/docs/Web/API/FileReader/readAsText
 
+    
+
     return await file_reader_promise;
 }
 
 
 // split into array
-function process( csvin) {
+function process( csvin, name) {
   let rows = csvin.split( "\n");
   const array = [];
 
   // for every row
   for (let r = 0; r < rows.length; r++) { 
-    if (rows[r] === "") {
+    if (rows[r] === "") { // skip if no data
       continue;
     }
+
+    // date, class, readings, assignments
     let words = rows[r].split( ","); // split the row
-    // add class
-    array.push(words); // add to the new array
+    zeroeth_word = words[0]; // get the first word
+    words.shift(); // everything but the first word
+
+    array.push([zeroeth_word, name, ...words]); // add to the new array
   }
 
   // TODO rearrange columns as necessary
@@ -80,25 +89,34 @@ function process( csvin) {
 
 
 // takes 2-d array and outputs csv file
-function arraytocsv( array, filename) {
+function array_to_csv( array) {
+  const filename = document.getElementById("schedule_output_name_csv").value
 
-  var csvfile = new Blob( array, {type: 'text/csv'});
-  if (navigator.msSaveBlob) { // IE 10+
-    navigator.msSaveBlob(csvfile, filename);
-  } else {
-    var link = document.createElement("a");
-    if (link.download !== undefined) { // feature detection
-        // Browsers that support HTML5 download attribute
-        var url = URL.createObjectURL(csvfile);
-        link.setAttribute("href", url);
-        link.setAttribute("download", filename);
-        link.style.visibility = 'hidden';
-        document.body.appendChild(link);
-        link.click();
-        document.body.removeChild(link);
+  if (array.length>0) {
+    // process into 
+    let file_contents = "";
+
+    // headings
+    file_contents = 'Date,Class,Readings,Assignments,\n';
+
+    // contents
+    for (let row=0; row<array.length; row++) {
+      for (let col=0; col<array[row].length; col++) {
+        file_contents += array[row][col] + ",";
+      }
+      file_contents += "\n";
     }
-  }
-  // https://gist.github.com/dhunmoon/d743b327c673b589e7acfcbc5633ff4b
+    console.log(file_contents);
+
+    const blob = new Blob([file_contents], { type: 'text/csv;charset=utf-8,' })
+    const objUrl = URL.createObjectURL(blob)
+    const link = document.createElement('a')
+    link.setAttribute('href', objUrl)
+    link.setAttribute('download', filename+'.csv')
+    link.textContent = 'Click to Download'
+
+    document.querySelector('body').append(link)
+  } // adapted from https://medium.com/@idorenyinudoh10/how-to-export-data-from-javascript-to-a-csv-file-955bdfc394a9
 }
 
 
@@ -133,12 +151,15 @@ function array_sort_date( array) {
 function displayarray( words) {
     console.log("begin display array function");
 
-    let table = '<table id="all-class-schedule"><thead><tr><th>Date</th><th>Readings</th><th>Assignments</th></tr></thead><tbody id="assignments">';
+    let table = '<table id="all-class-schedule"><thead><tr><th>Date</th><th>Class</th><th>Readings</th><th>Assignments</th></tr></thead><tbody id="assignments">';
 
     for (let i=0; i<words.length; i++) {
       let row = '<tr>';
 
-      for (cell=0; cell<words[i].length; cell++) {
+      // date
+      row += '<td>' + words[i][0].toLocaleString() + '</td>';
+
+      for (cell=1; cell<words[i].length; cell++) {
         cl = '<td>' + words[i][cell] + '</td>';
         row = row + cl;
       }
